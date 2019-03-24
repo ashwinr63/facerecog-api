0a56da7166b757a2518c9da295bb3192e0bea35c
ashwinr63 <8831182+ashwinr63@users.noreply.github.com>
2019-03-24 23:54:34 +0530

    heroku add

--- server.js
+++ server.js
@@ -49,6 +49,6 @@ app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 
 // listening to the app at port 3000;
-app.listen(3001, () => {
-  console.log("app is running at port 3001");
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running at port ${process.env.PORT}`);
 });
